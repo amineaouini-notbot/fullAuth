ea3ac54bc42704a8a2f0fa1b644ced2535fb119f
Geekyume <aminaouini565@gmail.com>
2023-12-04 00:31:45 +0100

    add neat front for register & signin

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,28 @@
+import Form from 'react-bootstrap/Form'
 const Register = () =>{
     return (
-        <div>
-            <a href="/signin">signin</a>
-            <a href="/register">register</a>
+        <div id="register-content" style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+          }}>
+            <div id="register" style={{ padding: '10px' ,border: '2px solid white', borderRadius: '7px'}}>
+                <Form>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label style={{color:'white'}}>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label  style={{color:'white'}}>password</Form.Label>
+                        <Form.Control type='password' placeholder="write your password" />
+                    </Form.Group>
+                </Form> 
+            
+
+                <a href="/signin">signin</a>
+            </div>
+            
         </div>
     )
 }
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,8 +1,30 @@
+import Form from 'react-bootstrap/Form'
 const SignIn = () =>{
     return (
         <div>
-            <a href="/signin">signin</a>
-            <a href="/register">register</a>
+            <div id="register-content" style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                }}>
+                <div id="register" style={{ padding: '10px' ,border: '2px solid white', borderRadius: '7px'}}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label style={{color:'white'}}>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label  style={{color:'white'}}>password</Form.Label>
+                            <Form.Control type='password' placeholder="write your password" />
+                        </Form.Group>
+                    </Form> 
+                
+
+                    <a href="/register">register</a>
+                </div>
+            
+            </div>
         </div>
     )
 }
