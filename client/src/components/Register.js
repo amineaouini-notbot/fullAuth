953ab88397a8554288f687953f62ef41470d2500
Geekyume <aminaouini565@gmail.com>
2023-12-04 22:08:07 +0100

    setup buttons to register and signin front

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,13 @@
 import Form from 'react-bootstrap/Form'
 import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
 const Register = () =>{
     const [password, onChangePass] = useState('')
     const [email, onChangeEmail] = useState('')
-    
+    const [checkPass, onChangeCheckPass] = useState('')
+    const onClick = () =>{
+
+    }
     return (
         <div id="register-content" style={{
             display: 'flex',
@@ -20,7 +24,14 @@ const Register = () =>{
                     <Form.Group className="mb-3" controlId="password">
                         <Form.Label  style={{color:'white'}}>password</Form.Label>
                         <Form.Control onChange={e => onChangePass(e.target.value)} type='password' placeholder="write your password" />
-                    </Form.Group>
+                    </Form.Group> 
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label  style={{color:'white'}}>password</Form.Label>
+                        <Form.Control onChange={e => onChangeCheckPass(e.target.value)} type='password' placeholder="write your password one more time" />
+                    </Form.Group> 
+                    <Button onclick={onClick} variant="primary" type="submit">
+                        Register
+                    </Button>
                 </Form> 
             
 
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 const SignIn = () =>{
     const [password, onChangePass] = useState('')
     const [email, onChangeEmail] = useState('')
-
+    const onClick = () => {
+        
+    }
     return (
         <div>
             <div id="register-content" style={{
@@ -22,6 +26,9 @@ const SignIn = () =>{
                             <Form.Label  style={{color:'white'}}>password</Form.Label>
                             <Form.Control onChange={e => onChangePass(e.target.value)} type='password' placeholder="write your password" />
                         </Form.Group>
+                        <Button onclick={onClick} variant="primary" type="submit">
+                            Sign In
+                        </Button>
                     </Form> 
                 
 
