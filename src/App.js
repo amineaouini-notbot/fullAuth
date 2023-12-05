f591f38dfc94b68efaade3dc220203f8553dd830
Geekyume <aminaouini565@gmail.com>
2023-12-05 23:14:23 +0100

    on logged in rediect to home if not redirect to register

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Register from './components/Register';
 import SignIn from './components/SignIn';
 import Home from './components/Home';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { redirect } from 'react-router-dom';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  
+  useEffect(()=>{
+    onAuthStateChanged(auth, user => {
+      if(user){ setLoggedIn(true) }
+      else { setLoggedIn(false) }
+
+    })
+  })
+
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='register' element={<Register/>}/>
-        <Route path='signin' element={<SignIn/>}/>
+        <Route path='/' element={loggedIn ? (<Home/>) : <Navigate to='/register' replace={true}/> }/>
+        <Route path='/register' element={loggedIn ? (<Navigate to={'/'} replace={true}/>):(<Register/>)}/>
+        <Route path='/signin' element={loggedIn ? (<Navigate to={'/'} replace={true}/>):(<SignIn/>)}/>
       </Routes>
     </div>
   );
