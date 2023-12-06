64bd5861b7489fcd766d040ec70503808434d7bb
Geekyume <aminaouini565@gmail.com>
2023-12-06 23:18:29 +0100

    add error alert on sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,11 +4,13 @@ import Button from 'react-bootstrap/Button'
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
 
 const SignIn = () =>{
     const navigate = useNavigate()
     const [password, onChangePass] = useState('')
     const [email, onChangeEmail] = useState('')
+    const [onError, setError] = useState(null)
     const onClick = () => {
         signInWithEmailAndPassword(auth, email, password)
         .then(userCredential =>{
@@ -16,8 +18,8 @@ const SignIn = () =>{
         })
         .catch(error =>{
             const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage)
+            const message = errorCode.split('/')[1].split('-').join(' ')
+            setError(message)
         })
     }
     return (
@@ -28,27 +30,30 @@ const SignIn = () =>{
                 justifyContent: 'center',
                 height: '100vh',
                 }}>
-                <div id="register" style={{ padding: '10px' ,border: '2px solid white', borderRadius: '7px'}}>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="email">
-                            <Form.Label style={{color:'white'}}>Email address</Form.Label>
-                            <Form.Control onChange={e => onChangeEmail(e.target.value)} type="email" placeholder="name@example.com" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="password">
-                            <Form.Label  style={{color:'white'}}>password</Form.Label>
-                            <Form.Control onChange={e => onChangePass(e.target.value)} type='password' placeholder="write your password" />
-                        </Form.Group>
-                    </Form> 
-                    <Button onClick={onClick} variant="primary" type="submit">
-                        Sign In
-                    </Button>
+                <div>
+                    {onError ? <Alert variant='danger'>{onError}</Alert> : <div></div>}
+                    <div id="register" style={{ padding: '10px' ,border: '2px solid white', borderRadius: '7px'}}>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label style={{color:'white'}}>Email address</Form.Label>
+                                <Form.Control onChange={e => onChangeEmail(e.target.value)} type="email" placeholder="name@example.com" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="password">
+                                <Form.Label  style={{color:'white'}}>password</Form.Label>
+                                <Form.Control onChange={e => onChangePass(e.target.value)} type='password' placeholder="write your password" />
+                            </Form.Group>
+                        </Form> 
+                        <Button onClick={onClick} variant="primary" type="submit">
+                            Sign In
+                        </Button>
 
-                    <hr style={{color: 'white'}}></hr>
-                
+                        <hr style={{color: 'white'}}></hr>
+                    
 
-                    <a href="/register">register</a>
-                </div>
+                        <a href="/register">register</a>
+                    </div>
             
+                </div>
             </div>
         </div>
     )
