cb4f9b3e024baf654276b59c30fb10e01fd10f41
Geekyume <aminaouini565@gmail.com>
2023-12-07 20:53:09 +0100

    setup wait counter to resend email

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,25 @@ import { Button } from "react-bootstrap";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Home = ({isVerified}) =>{
     const navigate = useNavigate()
+    const [waitCount, setwaitCount] = useState(null)
+    const verifyEmail = () =>{
+        if(!waitCount){
+            setwaitCount(30)
+        }
+        
+    }
+    useEffect(()=>{
+        if(waitCount > 0){
+            setTimeout(()=>{setwaitCount(waitCount-1)}, 1000)
+
+        } else {
+            setwaitCount(null)
+        }
+    }, [waitCount])
     const handleLogout = () =>{
         signOut(auth)
         .then(()=>{
@@ -18,9 +34,13 @@ const Home = ({isVerified}) =>{
                 <Button onClick={handleLogout} style={{float: 'right', marginRight: '5px', marginTop: '5px'}} variant="light">Logout</Button>
             </nav>
             <hr style={{color: 'white'}}></hr>
-            <div style={{borderBottom: isVerified ? '1px solid #bfffb3' : '1px solid #ff8a8a', width: '50vw', marginLeft: '25vw' }}>
+            <div style={{height: '6vh', borderBottom: isVerified ? '1px solid #bfffb3' : '1px solid #ff8a8a', width: '50vw', marginLeft: '25vw' }}>
                     <p style={{color:"white"}}>{isVerified ? 'Email is Verified' :"Email is not Verified"}</p>
                 
+            </div>
+            <div style={{marginTop: '30vh',width: "20vw", marginLeft: '40vw'}}>
+                <div onClick={verifyEmail} style={{userSelect: 'none' ,padding: '5px',border: '1px solid white', borderRadius: '5px',backgroundColor: 'white', width: '12vw', marginLeft: '5vw'}}>Resend Verification Email</div>
+                {waitCount? <p style={{color:"white"}}>wait: {waitCount} to resend email</p>: <div></div>}
             </div>
             
         </div>
